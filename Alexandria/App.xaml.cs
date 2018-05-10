8a76d11f711625a080b7daed7579ac7cb8142216
Fateblade <Fateblade@users.noreply.github.com>
2018-05-10 15:47:36 +0200

    initial
    project creation

---
+++ Alexandria/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Alexandria
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Bootstrapper _bootstrapper;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _bootstrapper = new Bootstrapper();
+            _bootstrapper.Run();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            _bootstrapper.Container.Dispose();
+        }
+    }
+}
---
+++ Alexandria/Shell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Alexandria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+            Show();
+        }
+    }
+}
