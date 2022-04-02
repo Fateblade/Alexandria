7f4838fcf9f7f275424af3acd292e8b4f93b8066
Fateblade <Fateblade@users.noreply.github.com>
2022-04-02 16:03:02 +0200

    add ui behaviour for multi selector controls

---
+++ UI/WPF/Base/UiBehaviour/MultiSelectorBehaviours.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Fateblade.Alexandria.UI.WPF.Base.UiBehaviour
+{
+    public static class MultiSelectorBehaviours
+    {
+        public static IList GetReadOnlyBindableSelectedItems(DependencyObject target)
+        {
+            return (IList)target.GetValue(ReadOnlyBindableSelectedItemsProperty);
+        }
+
+        public static void SetReadOnlyBindableSelectedItems(DependencyObject target, IList value)
+        {
+            target.SetValue(ReadOnlyBindableSelectedItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty ReadOnlyBindableSelectedItemsProperty =
+            DependencyProperty.RegisterAttached(
+                "ReadOnlyBindableSelectedItems",
+                typeof(IList),
+                typeof(MultiSelectorBehaviours),
+                new PropertyMetadata(default(IList), OnReadOnlyBindableSelectedItems));
+
+
+        private static void OnReadOnlyBindableSelectedItems(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MultiSelector selector = (MultiSelector)d;
+            IList listToUpdate = (IList)e.NewValue;
+
+            listToUpdate.Clear();
+            foreach (object toAdd in selector.SelectedItems)
+            {
+                listToUpdate.Add(toAdd);
+            }
+
+            selector.SelectionChanged += (_, args) =>
+            {
+                foreach (var removed in args.RemovedItems)
+                {
+                    listToUpdate.Remove(removed);
+                }
+
+                foreach (var added in args.AddedItems)
+                {
+                    listToUpdate.Add(added);
+                }
+            };
+
+
+        }
+    }
+}
---
+++ UI/WPF/Base/UiBehaviour/TextBoxEnterPressedCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Fateblade.Alexandria.UI.WPF.Base.UiBehaviour
+{
+    public static class TextBoxEnterPressedCommand
+    {
+        public static ICommand GetEnterPressedCommand(DependencyObject target)
+        {
+            return (ICommand)target.GetValue(EnterPressedCommandProperty);
+        }
+
+        public static void SetEnterPressedCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(EnterPressedCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty EnterPressedCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "EnterPressedCommand",
+                typeof(ICommand),
+                typeof(TextBoxEnterPressedCommand),
+                new PropertyMetadata(null, OnEnterKeyPressedCommandChanged));
+
+        private static void OnEnterKeyPressedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Control control = (Control)d;
+            ICommand newCmd = (ICommand)e.NewValue;
+
+            if (newCmd == null) return;
+
+            control.KeyUp += (sender, args) =>
+            {
+                if (args.Key == Key.Enter)
+                {
+                    control.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+                    if (newCmd?.CanExecute(null)==true)
+                    {
+                        newCmd.Execute(null);
+                    }
+                }
+            };
+
+        }
+    }
+}
