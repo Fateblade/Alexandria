94ed8b82a653734245e153ce78a4a7d7f3249c9f
Fateblade <Fateblade@users.noreply.github.com>
2022-07-31 13:43:34 +0200

    enable info dialog close button

--- UI/WPF/Modules/Alexandria.UI.WPF.Modules.ErrorDialog/Dialogs/InfoDialogViewModel.cs
+++ UI/WPF/Modules/Alexandria.UI.WPF.Modules.ErrorDialog/Dialogs/InfoDialogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Fateblade.Alexandria.UI.WPF.Base;
+using Prism.Commands;
 
 namespace Fateblade.Alexandria.UI.WPF.Client.Dialogs
 {
@@ -27,9 +28,22 @@ namespace Fateblade.Alexandria.UI.WPF.Client.Dialogs
             set => SetProperty(ref _infoText, value);
         }
 
+        public DelegateCommand CloseDialogCommand { get; }
+
+        public InfoDialogViewModel()
+        {
+            CloseDialogCommand = new DelegateCommand(closeDialog);
+        }
+
         protected override void InitializeDialog(InfoDialogCreationInformation information)
         {
             InfoText = information.InfoText;
         }
+
+        private void closeDialog()
+        {
+            RaiseRequestDialogClose(new InfoDialogResultInformation());
+        }
+
     }
 }
