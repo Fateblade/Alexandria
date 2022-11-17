d1ef769461ebede2acdbf92c5b01f4b1c4afe614
Fateblade <Fateblade@users.noreply.github.com>
2022-11-17 11:26:27 +0100

    refactor: move common converters into base library

---
+++ UI/WPF/Converters/Exceptions/ValueArgumentOfUnexpectedTypeException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fateblade.Alexandria.UI.WPF.ConvertersConverters.Exceptions
+{
+    [Serializable]
+    public class ValueArgumentOfUnexpectedTypeException : ArgumentException
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public ValueArgumentOfUnexpectedTypeException(object value) :
+            base($"Value is of unexpected type '{value?.GetType().FullName}'")
+        {
+        }
+
+        public ValueArgumentOfUnexpectedTypeException(object value, Type expectedType) :
+            base($"Value is of unexpected type '{value?.GetType().FullName}'. Expected '{expectedType.FullName}'")
+        {
+        }
+
+        protected ValueArgumentOfUnexpectedTypeException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ UI/WPF/Converters/ExistanceToVisibilityConverter.cs
@@ -0,0 +1,44 @@
+﻿using Fateblade.Alexandria.UI.WPF.ConvertersConverters.Exceptions;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Fateblade.Alexandria.UI.WPF.ConvertersConverters
+{
+    public enum ExistanceToVisibilityOption
+    {
+        DefaultValueVisible
+    }
+
+    public class ExistanceToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Visibility.Hidden;
+
+            if (GetDefault(value.GetType()) == value 
+                && !(parameter is ExistanceToVisibilityOption.DefaultValueVisible 
+                     || parameter?.ToString() == ExistanceToVisibilityOption.DefaultValueVisible.ToString()))
+            {
+                return Visibility.Hidden;
+            }
+            
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new IsOneWayConverterException();
+        }
+
+        public static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
+    }
+}
