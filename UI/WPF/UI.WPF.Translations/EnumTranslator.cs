3d0f9b1966e616273e19c21c30858072798e43ca
Fateblade <Fateblade@users.noreply.github.com>
2022-11-17 20:23:15 +0100

    Translations: move from alexandria ui specific to translation as common translations module

---
+++ UI/WPF/UI.WPF.Translations/EnumTranslator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Fateblade.Components.Logic.Foundation.Translation.Contract;
+
+namespace Alexandria.UI.WPF.Modules.CommonTranslations
+{
+    /// <summary>
+    /// Translates any Enum using EnumName.EnumValue as key
+    /// Flag combined enums cannot be translated using this translator
+    /// </summary>
+    internal class EnumTranslator : IEnumTranslator
+    {
+        private readonly ITranslationStringProvider _stringProvider;
+
+        public EnumTranslator(ITranslationStringProvider stringProvider)
+        {
+            _stringProvider = stringProvider;
+        }
+
+        public string Translate<TEnum>(TEnum toTranslate) where TEnum : Enum
+        {
+            return _stringProvider.GetString($"{typeof(TEnum).Name}.{toTranslate}");
+        }
+    }
+
+}
---
+++ UI/WPF/UI.WPF.Translations/IEnumTranslator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Alexandria.UI.WPF.Modules.CommonTranslations
+{
+    //TODO: Spezieller FlaggableEnumTranslator für kombinationen der auf struct ebene absichert und innerhalb nochmal auf basetype enum prüft
+
+    /// <summary>
+    /// Translates any Enum using EnumName.EnumValue as key
+    /// Flag combined enums cannot be translated using this translator
+    /// </summary>
+    public interface IEnumTranslator 
+    {
+        string Translate<TEnum>(TEnum toTranslate) where TEnum : Enum;
+    }
+}
