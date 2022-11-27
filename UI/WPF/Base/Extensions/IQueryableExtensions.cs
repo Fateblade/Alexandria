44f5217ef4f55ed6f3af3686d99e0cda73a2d73d
Fateblade <Fateblade@users.noreply.github.com>
2022-11-27 22:22:37 +0100

    add queryable extensions to base

---
+++ UI/WPF/Base/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Fateblade.Alexandria.UI.WPF.Base.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IQueryable<T> queryable)
+        {
+            return new ObservableCollection<T>(queryable);
+        }
+    }
+}
