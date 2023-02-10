7b1faf86e9fe22c2ce0ba399dc04a6616fe3406b
Fateblade <Fateblade@users.noreply.github.com>
2023-02-10 07:39:48 +0100

    wip action bar

--- UI/WPF/Controls/ActionBar/GroupingIconMenuBarItemControl.cs
+++ UI/WPF/Controls/ActionBar/GroupingIconMenuBarItemControl.cs
@@ -12,5 +12,15 @@ namespace Fateblade.Alexandria.UI.WPF.Controls.ActionBar
         }
         public static readonly DependencyProperty IconNameProperty =
             DependencyProperty.Register(nameof(IconName), typeof(string), typeof(GroupingIconMenuBarItemControl), new PropertyMetadata(default));
+
+        public bool IsSelected
+        {
+            get => (bool)GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(GroupingIconMenuBarItemControl), new PropertyMetadata(false));
+
+
     }
 }
