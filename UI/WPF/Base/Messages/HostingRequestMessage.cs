2f9cfbd7141264489b36dbd477d238ad4e9c8d22
Fateblade <Fateblade@users.noreply.github.com>
2023-04-09 22:50:02 +0200

    move hosting view model to alexandria

---
+++ UI/WPF/Base/Messages/HostingRequestMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fateblade.Alexandria.UI.WPF.Base.Messages
+{
+    public class HostingRequestMessage
+    {
+        public Type VmToHost { get; }
+        public string HostName { get; }
+
+        public HostingRequestMessage(Type vmToHost, string hostName)
+        {
+            VmToHost = vmToHost;
+            HostName = hostName;
+        }
+    }
+}
---
+++ UI/WPF/Base/Views/HostingViewModelBase.cs
@@ -0,0 +1,68 @@
+﻿using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.EventBrokerage;
+using Fateblade.Alexandria.UI.WPF.Base.Messages;
+using Prism.Ioc;
+using Prism.Mvvm;
+using System;
+
+namespace Fateblade.Alexandria.UI.WPF.Base.Views
+{
+    public abstract class HostingViewModelBase : BindableBase
+    {
+        private readonly IEventBroker _eventBroker;
+        private readonly IContainerProvider _container;
+        private readonly string _hostName;
+        private Type _currentlyHostedVmType;
+
+
+
+        private BindableBase _displayedContent;
+        public BindableBase DisplayedContent
+        {
+            get => _displayedContent;
+            private set => SetProperty(ref _displayedContent, value);
+        }
+
+
+
+        protected HostingViewModelBase(IEventBroker eventBroker, IContainerProvider container, string hostName)
+        {
+            _eventBroker = eventBroker;
+            _container = container;
+            _hostName = hostName;
+
+            _eventBroker.Subscribe<HostingRequestMessage>(handleHostingRequestMessage);
+        }
+
+        protected void resolveAndSwithToView(Type vmType)
+        {
+            if (_currentlyHostedVmType is { IsGenericType: true } && vmType.IsGenericType
+                && _currentlyHostedVmType.IsSubclassOf(typeof(HostingViewModelBase))
+                && vmType.IsSubclassOf(typeof(HostingViewModelBase)))
+            {
+                if (vmType.BaseType == _currentlyHostedVmType.BaseType
+                    && vmType.BaseType!=null)
+                {
+                    _currentlyHostedVmType = vmType;
+                    _eventBroker.Raise(
+                        new HostingRequestMessage(
+                            vmType.GenericTypeArguments[0],
+                            vmType.BaseType.Name));
+                }
+            }
+
+            _currentlyHostedVmType = vmType;
+            DisplayedContent = (BindableBase)_container.Resolve(vmType);
+        }
+
+
+
+        private void handleHostingRequestMessage(HostingRequestMessage obj)
+        {
+            if (obj.HostName == _hostName)
+            {
+                resolveAndSwithToView(obj.VmToHost);
+            }
+        }
+
+    }
+}
